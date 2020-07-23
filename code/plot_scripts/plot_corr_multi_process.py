671bb442f6d18ef377ae53a8817a899f170c2197
william laplante <williamlaplante123@gmail.com>
2020-07-23 16:13:26 -0400

    included parallel programming

---
+++ code/plot_scripts/plot_corr_multi_process.py
@@ -0,0 +1,51 @@
+import sys
+sys.path.append('/Users/williiamlaplante/Research/SynchrotronMaps/code')
+from corrfunc import compute_corr
+import matplotlib.pyplot as plt
+import numpy as np
+from processing import generate_ref_maps, generate_dust_maps
+import concurrent.futures
+import healpy as hp
+import time
+
+start = time.perf_counter()
+    
+#Fixing parameters
+nside = 128	
+resol = np.degrees(hp.nside2resol(nside)) #degrees 
+
+#retrieving maps
+ref_dict = generate_ref_maps(nside)
+dust_map = generate_dust_maps(nside)[0]
+
+#fixing plotting parameters
+plt.figure(figsize=(10,8))
+plt.xlabel('Degrees (arcmin)')
+plt.ylabel('Correlation')
+colors = ['red', 'pink', 'cyan', 'blue']
+
+x = np.exp(np.linspace(np.log(resol/1.2), np.log(2.5), np.log2(nside)+1)) #log scale 
+thicknesses = np.append((x[1:]-x[:-1]), (x[-1]-x[-2]))
+
+def compute_corr_wrap(p):
+	return compute_corr(*p)
+
+#computing the correlation values for all reference maps against 1998 dust map
+for zrange,color in zip(ref_dict.keys(), colors):
+	with concurrent.futures.ProcessPoolExecutor() as executor:
+	    args = ((ref_dict[zrange], dust_map, r, thick) for r, thick in zip(x,thicknesses))
+	    out = list(executor.map(compute_corr_wrap, args))
+	    y,err = zip(*out)
+	    plt.errorbar(x*60, list(y), yerr=list(err), color=color, label=zrange)
+
+
+plt.legend()
+plt.title('Correlation plot for dust maps with NSIDE='+str(nside))
+plt.xscale('log')
+
+filename = '/Users/williiamlaplante/Research/SynchrotronMaps/code/temp/corr_dust_maps'+str(nside)+'.%d.jpg' % time.time()
+plt.savefig(filename) 
+
+end = time.perf_counter()
+
+print('Finished in '+str(end-start)+' second(s)')
--- code/plot_scripts/plot_corr_script.py
+++ code/plot_scripts/plot_corr_script.py
@@ -4,7 +4,6 @@ from corrfunc import compute_corr
 import matplotlib.pyplot as plt
 import numpy as np
 from processing import generate_ref_maps, generate_dust_maps
-import concurrent.futures
 import healpy as hp
 import time
 
@@ -36,9 +35,7 @@ for nside in [128]:
 
     #computing the correlation values for all reference maps against 1998 dust map
     for zrange,color in zip(ref_dict,colors):
-        with concurrent.futures.ProcessPoolExecutor() as executor:
-            args = ((ref_dict[zrange], dust_map, r, thick) for r, thick in zip(x,thicknesses))
-            out = list(executor.map(lambda p:compute_corr(*args), args))
+            out = [compute_corr(ref_dict[zrange], dust_map, r, thick) for r, thick in zip(x,thicknesses)]
             y,err = zip(*out)
             plt.errorbar(x*60,list(y),yerr=list(err), color=color, label=zrange)
 
@@ -57,5 +54,4 @@ for nside in [128]:
 
 end = time.perf_counter()
 
-with open('time.txt', 'a+') as f:
-    f.write(str(end-start)+' seconds')
+print('Finished in '+str(end-start)+' second(s)')
