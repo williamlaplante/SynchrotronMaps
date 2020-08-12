ace5111cb49bcfbb1c8979e66e038fda1b5c4627
william laplante <williamlaplante123@gmail.com>
2020-08-12 10:07:35 -0400

    modified make_maps from csv to bin

--- code/scripts/computations/make_maps.py
+++ code/scripts/computations/make_maps.py
@@ -3,21 +3,23 @@ sys.path.append('../../')
 import numpy as np
 import pandas as pd
 from processing import generate_ref_maps_v2, generate_dust_map_1998
+from array import array
 
 full_path_ref = "../../../data/processed_maps/ref_maps/"
 full_path_dust = "../../../data/processed_maps/dust_maps/"
 
-for nside in [1024, 2048, 4096]:
+for nside in [128, 256, 512]:
     maps = generate_ref_maps_v2(nside)
     for m,z1,z2 in zip(maps, [0.1,0.3,0.5,1.2], [0.2,0.4,0.6,1.3]):
-        filename = str(z1) + 'z' + str(z2) + '_' + str(nside) + '_' + 'overdensity_field.csv'
-        pd.Series(m).to_csv(full_path_ref+filename, header='map_vals', index_label='pixel_num')
-        
+        filename = str(z1) + 'z' + str(z2) + '_' + str(nside) + '_' + 'overdensity_field.bin'
+        with open(full_path_ref + filename, "wb") as f:
+            array('d', m).tofile(f)
+            
     
     dust_map = generate_dust_map_1998(nside)
-    filename = 'ebv_' + str(nside) + '_1998.csv'
-    pd.Series(dust_map).to_csv(full_path_dust+filename, header='map_vals', index_label='pixel_num')
-
+    filename = 'ebv_' + str(nside) + '_1998.bin'
+    with open(full_path_dust + filename, "wb") as f:
+            array('d', m).tofile(f)
         
         
        
